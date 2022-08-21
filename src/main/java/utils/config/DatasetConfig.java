c1ecc701a02b76bd1d4090391bfadb2ff4bce782
jedishao23 <shao724854691@163.com>
2022-08-21 17:09:16 -0400

    dataset config

---
+++ src/main/java/utils/config/DatasetConfig.java
@@ -0,0 +1,7 @@
+package utils.config;
+
+public class DatasetConfig {
+
+  public static final String HOME_DIR = "C:\\Java\\ConBugLoc-dataset";
+  public static final String INDEX_DIR = HOME_DIR + "/Lucene-Index";
+}
