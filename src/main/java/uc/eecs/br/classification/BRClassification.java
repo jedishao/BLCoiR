d963adbbf7b01ecfca28449f58fdad89e4c4ebec
jedishao23 <shao724854691@163.com>
2022-08-28 15:56:35 -0400

    commit

---
+++ src/main/java/uc/eecs/br/classification/BRClassification.java
@@ -0,0 +1,27 @@
+package uc.eecs.br.classification;
+
+import java.util.ArrayList;
+
+public class BRClassification {
+     ArrayList<String> nl_content;
+     ArrayList<String> st_content;
+
+     public BRClassification(){
+          nl_content = new ArrayList<>();
+          st_content = new ArrayList<>();
+     }
+
+     public ArrayList<String> getNl_content() {
+          return nl_content;
+     }
+
+     public ArrayList<String> getSt_content() {
+          return st_content;
+     }
+
+     public boolean classification(ArrayList<String> content){
+
+          return false;
+     }
+
+}
