7fb19a05f976291980e7a2eb1571b9124fa6289c
jedishao23 <shao724854691@163.com>
2022-08-27 21:16:09 -0400

    Dataset Config

--- src/main/java/utils/config/DatasetConfig.java
+++ src/main/java/utils/config/DatasetConfig.java
@@ -4,4 +4,18 @@ public class DatasetConfig {
 
   public static final String HOME_DIR = "C:\\Java\\ConBugLoc-dataset";
   public static final String INDEX_DIR = HOME_DIR + "/Lucene-Index";
+  public static final String QUERY_DIR = HOME_DIR + "/Query";
+  public static final String QUERY_ST = "proposed-ST.txt";
+  public static final String QUERY_NL = "proposed-NL.txt";
+  public static final String QUERY_PE = "proposed-PE.txt";
+  public static String CORPUS_INDEX_KEY_MAPPING = HOME_DIR + "/Lucene-Index2File-Mapping";
+  public static String GOLDSET_DIR = HOME_DIR + "/Goldset";
+  public static String TOMCAT = "tomcat70";
+  public static String ECF = "ecf";
+  public static String DEBUG = "eclipse.jdt.debug";
+  public static String J_UI= "eclipse.jdt.ui";
+  public static String P_UI = "eclipse.pde.ui";
+  public static String CORE = "eclipse.jdt.core";
+
+
 }
