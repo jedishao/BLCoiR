316778ee977861747d303e2a569202d4798636a0
jedishao <shaoshuai2326@gmail.com>
2023-02-11 14:29:48 -0500

    code change

---
+++ src/java/utils/Corpus.java
@@ -0,0 +1,9 @@
+package utils;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Corpus {
+    public static List<String> CONCURRENCY =
+            Arrays.asList("lock", "concurrent", "concurrency", "wait");
+}
