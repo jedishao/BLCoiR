95851cee1a154958e0b347da5bbb69bc8f3de68d
jedishao <shaoshuai2326@gmail.com>
2023-02-11 16:01:35 -0500

    code change

--- src/java/evaluation/BaselineGen.java
+++ src/java/evaluation/BaselineGen.java
@@ -2,7 +2,10 @@ package evaluation;
 
 import edu.stanford.nlp.ling.IndexedWord;
 import edu.stanford.nlp.semgraph.SemanticGraph;
+import org.jgraph.graph.DefaultEdge;
+import org.jgrapht.graph.DefaultDirectedGraph;
 import uc.eecs.br.loader.BRLoader;
+import uc.eecs.core.query.GraphParser;
 import uc.eecs.nlp.DepManager;
 import uc.eecs.nlp.TextNormalizer;
 import utils.ResUtils;
@@ -14,7 +17,9 @@ import utils.config.QueryConfig;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -211,6 +216,54 @@ public class BaselineGen {
     fileWritter.close();
   }
 
+  public void genG(List<Integer> idList, String rep, String project) throws IOException {
+    DepManager depManager = new DepManager();
+    String path = DatasetConfig.QUERY_DIR + rep + "/" + project + "/baseline_entity.txt";
+    FileWriter fileWritter = new FileWriter(path, true);
+    int cus = 0;
+    for (int id : idList) {
+      cus++;
+      DefaultDirectedGraph<String, DefaultEdge> graph = new DefaultDirectedGraph<>(DefaultEdge.class);
+      String brPath = DatasetConfig.DATASET_DIR + rep + "/" + project + "/BR/" + id + ".txt";
+      List<String> con = BRLoader.loadBugReportList(brPath);
+      TextNormalizer tn = new TextNormalizer(con);
+      ArrayList<SemanticGraph> dependencies = depManager.getDependencies(tn.removeHttp());
+      for (SemanticGraph semanticGraph : dependencies) {
+        Set<String> noun = new HashSet<>();
+        for (IndexedWord root : semanticGraph.getRoots()) {
+          for (IndexedWord child : semanticGraph.getChildList(root)) {
+            checkVer(graph, root.word());
+            checkEdge(graph, root.word(), child.word());
+            getChildren(child, semanticGraph, graph);
+          }
+        }
+      }
+      GraphParser gp = new GraphParser(graph);
+      System.out.println(id + "\t" + gp.getQuery());
+    }
+  }
+  protected boolean getChildren(IndexedWord child, SemanticGraph semanticGraph, DefaultDirectedGraph<String, DefaultEdge> graph) {
+    if (semanticGraph.getChildList(child).isEmpty()) return false;
+    for (IndexedWord c1 : semanticGraph.getChildList(child)) {
+      checkVer(graph, child.word());
+      checkEdge(graph, child.word(), c1.word());
+      getChildren(c1, semanticGraph, graph);
+    }
+    return true;
+  }
+
+  protected void checkVer(DefaultDirectedGraph<String, DefaultEdge> graph, String j) {
+    if (!graph.containsVertex(j)) {
+      graph.addVertex(j);
+    }
+  }
+
+  protected void checkEdge(
+          DefaultDirectedGraph<String, DefaultEdge> graph, String from, String to) {
+    if (!graph.containsEdge(from, to)) {
+      graph.addEdge(from, to);
+    }
+  }
   public static void main(String[] args) throws IOException {
     int size = EvaluationConfig.BENCH4BL.length;
     for (int i = 0; i < size; i++)
