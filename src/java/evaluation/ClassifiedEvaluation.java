660d0db28301384775959f32c0941c8fcd9620ef
jedishao23 <shao724854691@163.com>
2023-02-11 20:09:23 -0500

    code change

--- src/java/evaluation/ClassifiedEvaluation.java
+++ src/java/evaluation/ClassifiedEvaluation.java
@@ -1,6 +1,5 @@
 package evaluation;
 
-
 import utils.ContentLoader;
 
 import utils.MiscUtility;
@@ -140,40 +139,40 @@ public class ClassifiedEvaluation {
     ClassifiedEvaluation ce = new ClassifiedEvaluation();
     //    String te = DatasetConfig.LTR;
     //    String path = "Query-Evaluation/All/our_PE.txt";
-    ce.calculateMAP();
+//    ce.calculateMAP();
     int i = 0;
     //    ce.collectResults(path, ClassificationID.LTR_PE);
-//        for (String s : EvaluationConfig.DATA) {
-//          String path1 = "Query-Evaluation/" + s + "/our_NL.txt";
-//          ce.collectResults(path1, EvaluationConfig.STP_ID.get(i));
-//          i++;
-//        }
+    //        for (String s : EvaluationConfig.DATA) {
+    //          String path1 = "Query-Evaluation/" + s + "/our_NL.txt";
+    //          ce.collectResults(path1, EvaluationConfig.STP_ID.get(i));
+    //          i++;
+    //        }
     String path1 = "Query-Evaluation/All/our+_st.txt";
     String path2 = "IRBL-Evaluation/All/" + DatasetConfig.BLIA + "_ST.txt";
     String path3 = "Query-Evaluation/All/our_st.txt";
     //    String path2 = "query/BLUiR_tomcat.txt";
     Map<String, List<Integer>> l1 = ce.mapping(path1);
     Map<String, List<Integer>> l2 = ce.mapping(path2);
-//    Map<String, List<Integer>> l3 = ce.mapping(path3);
+    //    Map<String, List<Integer>> l3 = ce.mapping(path3);
 
     List<Integer> list1 = CAMEL_STP;
-//    for (String s : l1.keySet()) {
-//      if (list1.contains(Integer.parseInt(s)))
-//        System.out.println(s+":"+l1.get(s));
-//    }
-//    System.out.println("=====================================");
-//    for (String s : l2.keySet()) {
-//      if (list1.contains(Integer.parseInt(s)))
-//        System.out.println(s+":"+l2.get(s));
-//    }
-    String p = "Query-Evaluation/All/our+_ST.txt";
-//    List<String> ll = ContentLoader.getAllLinesList(p);
-//    for (String s : ll) {
-//      if (BENCH4BL_STN.contains(Integer.parseInt(s.split("\t")[0])))
-//        System.out.println(s);
-//    }
+    //    for (String s : l1.keySet()) {
+    //      if (list1.contains(Integer.parseInt(s)))
+    //        System.out.println(s+":"+l1.get(s));
+    //    }
+    //    System.out.println("=====================================");
+    //    for (String s : l2.keySet()) {
+    //      if (list1.contains(Integer.parseInt(s)))
+    //        System.out.println(s+":"+l2.get(s));
+    //    }
+
+    String p = "Query/" + DatasetConfig.LTR + "/" + DatasetConfig.PLATFORM + "/our+.txt";
+    List<String> ll = ContentLoader.getAllLinesList(p);
+    for (String s : ll) {
+      if (PLATFORM_STN.contains(Integer.parseInt(s.split("\t")[0]))) System.out.println(s);
+    }
 
     String path = "Query-Evaluation/All/our+_STN1.txt";
-//    ce.calculateTop(ce.mapping(path), DatasetConfig.STN_SIZE);
+    //    ce.calculateTop(ce.mapping(path), DatasetConfig.STN_SIZE);
   }
 }
